e1899b53fa466449599d9ba215a4b34c8e476ef1
Ekaterina Antonova <missis.katia-antonova@yandex.ru>
2024-08-17 11:11:18 +0300

    Внесение правок

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,15 @@
 import Game from './game/game';
 
 document.addEventListener('DOMContentLoaded', () => {
+    const main = document.createElement('main');
+    main.classList = 'field';
+    document.body.appendChild(main);
+    for(let i = 0; i < 16; i++) {
+        let div = document.createElement('div');
+        div.classList = 'field-item';
+        main.appendChild(div);
+    }
+
     const game = new Game(document.querySelectorAll('.field-item'));
     setInterval(() => {
         game.movingCharacter();
